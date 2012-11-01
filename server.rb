6935c49d623820039c37c5b89aa941486d7caaae
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-11-01 14:37:53 -0400

    merged users and wiki

--- server.rb
+++ server.rb
@@ -1,11 +1,70 @@
+require "redis"
 require "sinatra"
 require "sinatra/reloader" if development?
-require "redis"
+require "json"
 
 enable :sessions
-$login_db = Redis.new
+
+$pages_db = Redis.new
+
+def get_pages
+  pages = []
+  $pages_db.hkeys("pagehash").each do |page_title|
+    pages << $pages_db.hget("pagehash", page_title)
+  end
+
+  return pages
+end
 
 get "/" do
+  pages = get_pages()
+  erb :home,
+    locals: {pages: pages}
+end
+
+get "/add_entry" do
+  erb :add_entry
+end
+
+post "/add_entry" do
+  if $pages_db.hkeys("pagehash").include?(params["title"]) # ensures pages with same title
+    redirect to "/"                                        # are not added
+  else
+    page = {title: params["title"],
+            author: params["author"],
+            body: params["body"],
+            date_added: Time.now,
+            date_modified: nil,
+            last_modified_by: nil}
+    $pages_db.hset("pagehash", params["title"], page.to_json)
+    redirect to "/"
+  end
+end
+
+get "/view/:title" do
+  page = $pages_db.hget("pagehash", params[:title])
+  erb :view_entry,
+    locals: {p: page}
+end
+
+get "/edit/:title" do
+  page = $pages_db.hget("pagehash", params[:title])
+  erb :add_entry,
+    locals: {p: page}
+end
+
+post "/edit_entry" do
+  page = JSON.parse($pages_db.hget("pagehash", params["title"]))
+  page["body"] = params["body"]
+  page["date_modified"] = Time.now
+  page["last_modified_by"] = params["author"]
+  $pages_db.hset("pagehash", page["title"], page.to_json)
+  redirect to "/"
+end
+
+# user login stuff
+
+get "/login" do
   erb :login
 end
 
@@ -15,7 +74,7 @@ post "/login" do
     session["username"] = params["username"]
     redirect to "/special"
   else
-    redirect to "/"
+    redirect to "/login"
   end
 end
 
